5250fb7c735b83e33a94671f0f2d0ac81751011a
mxlisows <mxlisows@molsoncoors.com>
2019-11-14 17:02:51 +0100

    Initial commit with webpack & babel enabled.

---
+++ server.js
@@ -0,0 +1,18 @@
+import { join } from 'path';
+import express from 'express';
+const debug = require('debug')('edu:server');
+
+const app = express(),
+    DIST_DIR = __dirname,
+    HTML_FILE = join(DIST_DIR, 'index.html');
+
+app.use(express.static(DIST_DIR));
+
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    debug(`App listening to ${PORT}....`);
+});
